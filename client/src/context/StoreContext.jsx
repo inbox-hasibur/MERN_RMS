6e31f8626e304550310d67d07004680298a9bcb8
abdify <abdullah.fahimm08@gmail.com>
2025-05-28 23:22:32 +0600

    fix: frontend issues fix

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -1,63 +1,60 @@
-import axios from "axios";
 import { createContext, useState, useEffect } from "react";
+import { food_list } from "../assets/assets";
+import api from "../utils/api";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:3000";
-  const [token,setToken] = useState("");
+  const url = "http://localhost:4000";
+  const [token, setToken] = useState("");
   const [foodList, setFoodList] = useState(food_list);
 
   const addToCart = async (itemId) => {
     if (!cartItems[itemId]) {
-      setCartItems((prev) => ({...prev, [itemId]: 1}));
-    }
-    else {
-      setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}));
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
     if (token) {
-      await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
+      await api.post("/cart/add", { itemId });
     }
-  }
+  };
 
-  const removeFromCart = async (itemId) => { 
-   setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1}));
-   if  (token){
-    await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
-   }
-  }
-  
+  const removeFromCart = async (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    if (token) {
+      await api.post("/cart/remove", { itemId });
+    }
+  };
+
   const getTotalCartAmount = () => {
     let totalAmount = 0;
-    for (const item in cartItems) 
-    { 
+    for (const item in cartItems) {
       if (cartItems[item] > 0) {
         let itemInfo = food_list.find((food) => food._id === item);
         totalAmount += itemInfo.price * cartItems[item];
-      } 
+      }
     }
     return totalAmount;
-  }
+  };
 
   const fetchFoodList = async () => {
-    const response = await axios.get(url+"/api/foods/list");
+    const response = await api.get("/food/list");
     setFoodList(response.data.data);
-  }
+  };
 
-  const loadCartData = async (token) => {
-    const response = await axios.post(url+"/api/cart/get",{},{headers:{ token}});
+  const loadCartData = async () => {
+    const response = await api.post("/cart/get", {});
     setCartItems(response.data.cartData);
-  }
+  };
 
   useEffect(() => {
-    
     async function loadData() {
       await fetchFoodList();
       if (localStorage.getItem("token")) {
         setToken(localStorage.getItem("token"));
-        await loadCartData(localStorage.getItem("token"));
+        await loadCartData();
       }
     }
     loadData();
@@ -72,14 +69,14 @@ const StoreContextProvider = (props) => {
     getTotalCartAmount,
     url,
     token,
-    setToken
-  }
-  
+    setToken,
+  };
+
   return (
     <StoreContext.Provider value={contextValue}>
       {props.children}
     </StoreContext.Provider>
-  )
-}
+  );
+};
 
 export default StoreContextProvider;
--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,13 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv' //
-import { connectDB } from './config/db.js';
-import User from './models/userModel.js'; //
-import foodRouter from './routes/foodRoute.js';
-import userRouter from './routes/userRoute.js';
-import 'dotenv/config'; 
-import cartRouter from './routes/cartRoute.js';
-import orderRouter from './routes/orderRoute.js';
-
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv"; //
+import { connectDB } from "./config/db.js";
+import User from "./models/userModel.js"; //
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 dotenv.config();
 const app = express();
@@ -20,42 +19,42 @@ app.use(cors());
 //   res.send('Welcome to server!');
 // });
 
-app.post('/api/users/register', async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' });
-        }
-        const newUser = new User({name,email,password});
-        await newUser.save() 
-        res.status(201).json({ 
-            message: 'User registered', user: newUser 
-        });
-    } catch (error) {
-        console.error('Server error:', error);
-        res.status(500).json({ 
-            message: 'Error registering user', error: error.message 
-        });
+app.post("/api/users/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
     }
+    const newUser = new User({ name, email, password });
+    await newUser.save();
+    res.status(201).json({
+      message: "User registered",
+      user: newUser,
+    });
+  } catch (error) {
+    console.error("Server error:", error);
+    res.status(500).json({
+      message: "Error registering user",
+      error: error.message,
+    });
+  }
 });
 
 // API Endpoints
 app.use("/api/food", foodRouter);
-app.use("/images", express.static('uploads'));
+app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/orders", orderRouter);
 
 // app.get('/test', (req, res) => {
-//     res.status(200).send({ 
+//     res.status(200).send({
 //         message: 'GET: Test API is working!'
 //     });
 // });
 
-
-app.listen(3000, () => {
-    connectDB();
-    console.log('Server running at http://localhost:3000/');
+app.listen(4000, () => {
+  connectDB();
+  console.log("Server running at http://localhost:3000/");
 });
-
